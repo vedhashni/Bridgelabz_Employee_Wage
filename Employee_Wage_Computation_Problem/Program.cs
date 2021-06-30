9974a22f4b926ddb0025f928868daed6e67e862e
[vedhashni] <[vedhavel.1234@gmail.com]>
2021-06-30 06:22:35 +0530

    First Commit

---
+++ Employee_Wage_Computation_Problem/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Employee_Wage_Computation_Problem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome To Employee_Wage_Computation!");
+        }
+    }
+}
